2fdad1c4bb70b93cfa4c229ed4913dd81dd85979
zhuhgjava <zhuhgjava@sina.com>
2021-05-20 17:00:42 +0800

    test

---
+++ src/test/java/com/zhg/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhg.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
