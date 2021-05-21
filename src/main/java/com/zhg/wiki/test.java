9b1ad658c9e8641f955cdaacc22e51b4dc391eb2
zhuhgjava <zhuhgjava@sina.com>
2021-05-21 18:02:29 +0800

    test2

--- src/main/java/com/zhg/wiki/test.java
+++ src/main/java/com/zhg/wiki/test.java
@@ -3,9 +3,11 @@ package com.zhg.wiki;
 public class test {
     public static void main(String[] args) {
         System.out.println("aaa");
+
+
         System.out.println("aaa");
 
-        
+
         System.out.println("aaa");
     }
 }
